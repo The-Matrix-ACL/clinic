01211734c92aa3f27a566545f2b3ac101e6ec664
mazenessam26 <127431425+mazenessam26@users.noreply.github.com>
2023-10-15 01:23:51 +0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,219 @@
+const React= require("react");
+const  Express = require("express");
+const cors= require('cors');
+//import  mongoose  from "mongoose";
+const ejs = require("ejs");
+var bodyParser = require('body-parser')
+//import Createadminstrator from './Frontend/AdminstratorForm.ejs';
+//import bars from './bars'
+//import './patientRegister.css';
+//import './theme.css';
+const mongoose = require("mongoose");
+const dotenv =  require('dotenv');
+const path =  require('path');
+const ReactDOM = require('react-dom');
+const {createUser, createDoctor, createAdminstrator, deleteDoctor, deleteAdminstrator, deleteUser, getDoctor,editDoctorInfo,filterByDateOrStatus,searchForPatient, getUsers, getDoctors, addPackage, updatePackage, deletePackage,addFamilyInfo,getFamilyMembers, searchForDoctor, searchForDoctorspeciality, searchForDoctordate} =require("./Routes/userController.js");
+
+/*const React = require('react');
+import ReactDOM = from'react-dom';
+
+const express = require("express");
+const mongoose = require('mongoose');
+mongoose.set('strictQuery', false);
+require("dotenv").config();
+const {createUser, createDoctor, createAdminstrator, deleteDoctor, deleteAdminstrator, deleteUser, getDoctor} = require("./Routes/userController.js");
+const MongoURI = process.env.MONGO_URI ;
+
+  const Adminstratorform = require('./Frontend/AdminstratorForm');*/
+
+  //import Aform from './Frontend/Aform';
+
+//Adminstratorform.call();
+mongoose.set('strictQuery', false);
+dotenv.config();
+const MongoURI = process.env.MONGO_URI ;
+const app = Express();
+const port = process.env.PORT || "3000";
+app.use(cors({origin: 'http://localhost:3000',  // Replace with your frontend's URL
+methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+credentials: true,}));
+//const __filename = new URL(import.meta.url).pathname;
+//const __dirname = path.dirname(__filename); 
+//const user = require('./Models/User.js');
+//const createadminstrator = require('./Frontend/AdminstratorForm');
+mongoose.connect(MongoURI)
+.then(()=>{
+  console.log("MongoDB is now connected!")
+// Starting server
+ app.listen(port, () => {
+    console.log(`Listening to requests on http://localhost:3000`);
+  })
+})
+.catch(err => console.log(err));
+
+
+
+//.............................................//
+
+/*app.post("/home", (req, res) => {
+  res.status(200).send(Createadminstrator.call());
+});
+const temp = '<h1>Hello <%= name %></h1>'; 
+app.get('/', (req, res) => {
+  const data = { name: 'World' };
+  const html = ejs.render(temp, data);
+  res.send(html);
+});
+app.set('view engine', 'ejs');
+
+app.post('/createAdminstrator',(req,res)=>{
+  res.send(ejs.render('AdminstratorForm'));
+})*/
+
+/*app.listen(3001, () => {
+  console.log(' Server listening on port 3000 ');
+}); */
+
+app.set('view engine', 'ejs');
+//app.set('views', path.join(__dirname, 'Frontend'));
+// #Routing to userController here
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
+
+//app.engine('html', require('jsx').renderFile);
+app.use(Express.json())
+app.use(Express.urlencoded({ extended: true }));
+
+// Serving static files (like index.js)
+app.use(Express.static(path.join(__dirname, 'public')));
+
+app.get('/createAdminstrator', (req, res) => {
+  res.render('AdminstratorForm',{ formData: { Username: '', Password: '' } });
+});
+
+app.get('/createDoctor', (req, res) => {
+  res.render('DoctorForm',{ formData: { Username: '',Name: '',Email: '',Password: '',DateOfBirth: '',Hospital:'',HourlyRate:0,EducationalBackground:'',Speciality:'' } });
+});
+
+app.get('/createUser', (req, res) => {
+  res.render('PatientForm',{ formData: { Username: '',Name: '',Email: '',Password: '',DateOfBirth: '',Gender:'',MobileNumber:0,EmergencyContactFullName:'',EmergencyContactNumber:0 } });
+});
+
+app.get('/deleteDoctor', (req, res) => {
+  res.render('DeleteDoc',{ formData: { Username: ''} });
+});
+
+app.get('/deleteAdminstrator', (req, res) => {
+  res.render('DeleteAdmin',{ formData: { Username: ''} });
+});
+
+app.get('/deletePatient', (req, res) => {
+  res.render('DeletePatient',{ formData: { Username: ''} });
+});
+
+app.get('/getDoctor', (req, res) => {
+  res.render('getDoctor',{ formData: { Username: ''} });
+});
+
+app.get('/', (req, res) => {
+  res.render('MainRegisterPage');
+});
+
+app.get('/prescriptions', (req, res) => {
+  res.render('prescriptions');
+});
+
+app.get('/homepageAdmin', (req, res) => {
+  res.render('homepageAdmin');
+});
+
+app.get('/homepagePatient', (req, res) => {
+  res.render('homepagePatient');
+});
+
+app.get('/updatedoctorInfo', (req, res) => {
+  res.render('updateDoctor',{ formData: { Email: '',newEmail:'',newHospital:'',newHourlyRate:0 } });
+});
+
+app.get('/filterappointmentsuser', (req, res) => {
+  res.render('FilterAppointmentsUser');
+});
+
+app.get('/filterappointmentsdoctor', (req, res) => {
+  res.render('FilterAppointmentsDoctor');
+});
+
+app.get('/homepageDoctor', (req, res) => {
+  res.render('homepageDoctor');
+});
+
+app.get('/getPatient', (req, res) => {
+  res.render('getPatient',{ formData: { Name: ''} });
+});
+
+app.get('/getallPatients', (req, res) => {
+  res.render('getallPatients');
+});
+
+app.get('/getallDoctors', (req, res) => {
+  res.render('getallDoctors');
+});
+
+app.get('/addPackage', (req, res) => {
+  res.render('addPackage',{ formData: { Username: '',HealthPackage:''} });
+});
+
+app.get('/updatePackage', (req, res) => {
+  res.render('PackageUpdate',{ formData: { Username: '',newHealthPackage:''} });
+});
+
+app.get('/deletePackage', (req, res) => {
+  res.render('PackageDelete',{ formData: { Username: ''} });
+});
+
+app.get('/addFamilyMember', (req, res) => {
+  res.render('addFamilyMember',{ formData: { Username: '',Name:'',NationalID:0,Age:0,Gender:'',Relation:''} });
+});
+
+app.get('/getFamilyMember', (req, res) => {
+  res.render('getFamilyMembers',{formData: {Username:''}});
+});
+
+app.get('/searchDoctor', (req, res) => {
+  res.render('searchDoctorName',{formData: {Name:''}});
+});
+
+app.get('/searchDoctorspeciality', (req, res) => {
+  res.render('searchDoctorSpeciality',{formData: {Speciality:''}});
+});
+
+app.get('/searchDoctordate', (req, res) => {
+  res.render('searchDoctordate',{formData: {Avaliable:''}});
+});
+
+app.post("/addUser",createUser);
+app.post("/createDoctor",createDoctor);
+app.post("/createAdminstrator",createAdminstrator,);
+app.post("/deleteUser",deleteUser);
+app.post("/deleteDoctor",deleteDoctor);
+app.post("/deleteAdminstrator",deleteAdminstrator);
+app.post("/getDocto",getDoctor);
+app.post("/editDoctorInfo", editDoctorInfo);
+app.get('/filterbydateorstatus',filterByDateOrStatus);
+app.post("/patientsearch",searchForPatient);
+app.get("/getPatients",getUsers);
+app.get("/getDoctors",getDoctors);
+app.post("/addPackage",addPackage);
+app.post("/updatePackage",updatePackage);
+app.post("/deletePackage",deletePackage);
+app.post("/addFamilyMember",addFamilyInfo);
+app.post("/getFamilyMember",getFamilyMembers);
+app.post("/doctorSearch",searchForDoctor);
+app.post("/doctorSearchspeciality",searchForDoctorspeciality);
+app.post("/doctorSearchdate",searchForDoctordate);
+//app.post("/abc",Createadminstrator);
+
+
+
